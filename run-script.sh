52521f6025c84bf744e707ceda78ffb46b96f74a
Eyal159 <74238936+Eyal159@users.noreply.github.com>
2021-09-12 21:35:07 +0300

    Update run-script.sh

--- run-script.sh
+++ run-script.sh
@@ -1,1 +1,13 @@
+source_pass=""
+source_host=""
+source_db_name=""
+source_user=""
+source_port=""
 
+dest_pass=""
+dest_host=""
+dest_db_name=""
+dest_user=""
+dest_port=""
+
+./dump-restore.sh $source_pass $source_host $source_db_name $source_user $source_port $dest_pass $dest_host $dest_db_name $dest_user $dest_port
